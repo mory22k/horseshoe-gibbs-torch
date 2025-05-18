e52337aa28970b03c87c74275bd9febadb885eec
mory22k <mory22k@dc.tohoku.ac.jp>
2025-05-18 11:24:17 +0900

    build(task): fix precommmits

--- .scripts/prepare-commit/src.sh
+++ .scripts/prepare-commit/src.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Checks for changes in the ./src directory and prepares a commit if changes are detected.
 if git diff --cached --quiet -- ./src; then
   echo "No changes detected in ./src"
